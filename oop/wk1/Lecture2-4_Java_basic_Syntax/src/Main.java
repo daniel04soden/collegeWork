3591047482910f943ff46338a96f1b04f7fdd161
daniel04soden <dsoden09@gmail.com>
2024-09-12 11:33:47 +0100

    added java files to lectures

---
+++ oop/wk1/Lecture2-4_Java_basic_Syntax/src/Main.java
@@ -0,0 +1,56 @@
+// Author: Daniel Soden
+// Purpose: First Java program
+
+/**
+ * The main class of our project.<br>.
+ */
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+public class Main {
+
+    //-------------------------------------
+    //	myFunction
+    //-------------------------------------
+    public static void myFunction(){
+        // 1. Print a message by the standard output
+        System.out.print("Hello World!");
+
+        //2. We can print the value of a variable
+        int x = 3;
+        System.out.println(x);
+
+        //3. We can print to a file
+
+        try {
+            //1. We create the file for writing
+            PrintWriter f = new PrintWriter("my_file.txt", "UTF-8");
+            f.println("Hello, now I'm printing to a file from Java");
+            f.close();
+        }
+        catch (Exception e) {
+            System.out.println("Sorry but the file cannot be created for writing to it");
+        }
+
+        //4. We can ask users for some input and print it later.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter something: ");
+        String myText = sc.nextLine();
+        System.out.println("You entered " + myText);
+				sc.close();
+    }
+
+    //-------------------------------------
+    //	main
+    //-------------------------------------
+    /**
+     * Main Method:<br>
+     * Main method of the class.<br>
+     * @param args: We will run the program parameter free, so do not worry about it.
+     */
+    public static void main(String[] args) {
+        myFunction();
+    }
+
+}
+
