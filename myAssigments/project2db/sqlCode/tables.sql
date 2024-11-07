4b77763a1d6d71837bf2f59e567da3298e378df1
DanielS <dsoden09@gmail.com>
2024-11-07 23:20:44 +0000

    nearly done databases project

--- myAssigments/project2db/sqlCode/tables.sql
+++ myAssigments/project2db/sqlCode/tables.sql
@@ -1,29 +1,62 @@
 USE horseClinic;
 
 CREATE TABLE IF NOT EXISTS staff (
-  attribute(s)
+  VARCHAR(20) UNIQUE staffNo,
+  VARCHAR(20) clinicNo,
+  VARCHAR(15) fName,
+  VARCHAR(15) lName,
+  VARCHAR(30) address,
+  VARCHAR(20) telNo,
+  DOUBLE(10,2) salary,
+  DATE DOB,
+  PRIMARY KEY staffNo,
+  Foreign Key (clinicNo) REFERENCES (clinic)
 );
 
 
 CREATE TABLE IF NOT EXISTS clinic (
-  attribute(s)
+    VARCHAR(20) UNIQUE clinicNo,
+  VARCHAR(20) staffNo,
+  VARCHAR(30) address,
+  VARCHAR(20) telNo,
+  PRIMARY KEY clinicNo,
+  Foreign Key (staffNo) REFERENCES (staff)
 );
 
 
 CREATE TABLE IF NOT EXISTS owner (
-  attribute(s)
+  VARCHAR(20) UNIQUE ownerNo,
+  INT(3) qtyHorses,
+  VARCHAR(15) fName,
+  VARCHAR(15) lName,
+  VARCHAR(30) address,
+  VARCHAR(20) telNo,
+  PRIMARY KEY ownerNo
 );
 
 
 CREATE TABLE IF NOT EXISTS horse (
-  attribute(s)
+  VARCHAR(20) UNIQUE horseID,
+  VARCHAR(20) ownerNo,
+  VARCHAR(20) clinicNo,
+  VARCHAR(10) colour,
+  VARCHAR(15) horseName,
+  PRIMARY KEY horseID,
+  Foreign Key (ownerNo) REFERENCES (owner),
+  Foreign Key (clinicNo) REFERENCES (clinic)
 );
 
 
 CREATE TABLE IF NOT EXISTS consultation (
-  attribute(s)
+  VARCHAR(20) consultNo,
+  VARCHAR(20) horseNo,
+  PRIMARY KEY consultNo,
+  Foreign Key (horseNo) REFERENCES (horse)
 );
 
 CREATE TABLE IF NOT EXISTS treatment (
-  attribute(s)
+  VARCHAR(20) treatmentNo,
+  VARCHAR(20) consultNo
+  PRIMARY KEY treatmentNo,
+  Foreign Key (consultNo) REFERENCES (consultation)
 );
