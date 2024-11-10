eeaf72e1efd3bc298a872000a591d034ac094991
Daniel SodenPC <dsoden09@gmail.com>
2024-11-10 23:25:44 +0000

    merge working fully, sort very close

--- myAssigments/Assignment_LDSA_2/MergeSort.java
+++ myAssigments/Assignment_LDSA_2/MergeSort.java
@@ -2,37 +2,49 @@ public class MergeSort {
 
   private static int[] merge(int[] A1, int[] A2) {
     // TASK 2.A.a
-<<<<<<< HEAD
 
-    return A1;
-=======
     int n = A1.length + A2.length; // The new arrays length is the two arrays sum
     int i = 0; // Index for A1
     int j = 0; // Index for A2
     int[] A3 = new int[n]; // New array with the capacity of n
 
-    for (int k = 0; k < n; k++) {
-      if (A1[i] <= A2[j]) {
+    for (int k = 0; k < n; k++) { // K is the iterative index
+      if (i <= j) { // If the indicator of the first array is less than the indicator for the second
+                    // array
         A3[k] = A1[i];
-        i++;
-      } else {
+        i++; // Increase indicator
+      } else { // Otherwise assign K to the value at array 2
         A3[k] = A2[j];
         j++;
       }
 
     }
     return A3;
->>>>>>> fb10922 (added partial merge sort code, nearly there)
   }
 
   public static int[] mergesort(int[] A) {
     // TASK 2.A.b
-    // if (A.length <= 1) {
-    // return A;
-    // }
-    //
-    throw new RuntimeException("bruh");
+    int n = A.length;
 
+    if (n <= 1) {
+      return A;
+    } else {
+      int middle = (n / 2);
+      int[] ALeft = new int[middle];
+      int[] ARight = new int[n - middle];
+
+      for (int i = 0; i < middle; i++) {
+        ALeft[i] = A[i];
+      }
+
+      for (int i = middle; i < n; i++) {
+        ARight[i - middle] = A[i];
+      }
+
+      int[] sortA1 = mergesort(ALeft);
+      int[] sortA2 = mergesort(ARight);
+      return merge(sortA1, sortA2);
+    }
   }
 
   private static void print(int[] A) {
