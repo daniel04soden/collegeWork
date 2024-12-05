d771db9a9a35a6f6cfc6c7b8c6dbe8b9ef552803
daniel04soden <dsoden09@gmail.com>
2024-12-05 12:51:31 +0000

    added new oop files

---
+++ myAssigments/tofinish/A03DS/src/main/java/org/example/Agent.java
@@ -0,0 +1,15 @@
+public class Agent {
+    private final int id;
+
+    public Agent(int _id){
+        this.id = _id;
+    }
+
+    public final int getId(){
+        return this.id;
+    }
+
+    public boolean checkEquals(Agent A){
+        return this.id == A.id;
+    }
+}
