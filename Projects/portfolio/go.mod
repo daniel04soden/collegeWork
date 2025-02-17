5e1ce324fa086fa74f90a5bcc9b5facad88e726c
dsoden <dsoden09@gmail.com>
2025-02-17 23:56:28 +0000

    added concepts for future user ticket system

--- Projects/portfolio/go.mod
+++ Projects/portfolio/go.mod
@@ -1,3 +1,5 @@
 module portfolio
 
 go 1.23.6
+
+require golang.org/x/crypto v0.33.0 // indirect
--- Projects/portfolio/main.go
+++ Projects/portfolio/main.go
@@ -5,11 +5,26 @@ import (
 	"log"
 	"net/http"
 	"text/template"
+	"time"
+	// "golang.org/x/crypto/bcrypt" - will need soon for password encryption
 )
 
-type Film struct {
-	Title    string
-	Duration float64
+type User struct {
+	id        int64
+	firstName string
+	lastName  string
+	DOB       time.Time // Only date
+	email     string
+	username  string
+	password  string
+}
+
+type Ticket struct { // Tickets of features to add to site/other queries
+	concernedUser User      // User that made the ticket
+	timeOfTicket  time.Time // Date and time of ticket
+	ticketID      int64     // Unique id of ticket
+	subject       string    // Overall subject for ticket
+	content       string    // Content of what they want in the ticket
 }
 
 func main() {
@@ -17,14 +32,12 @@ func main() {
 	fmt.Println("Sever started on port ", port)
 	initPage := func(w http.ResponseWriter, r *http.Request) {
 		templ := template.Must(template.ParseFiles("index.html"))
-		films := map[string][]Film{
-			"Films": {
-				{Title: "Jurassic World", Duration: 23.8},
-				{Title: "Jurassic Park", Duration: 23.8},
-				{Title: "Fallen Kingdom", Duration: 23.8},
+		users := map[string][]User{
+			"Users": {
+				{"1", "Daniel", "Soden",,"dsoden09@gmail.com","daniel04soden",""},
 			},
 		}
-		err := templ.Execute(w, films)
+		err := templ.Execute(w, users)
 		if err != nil {
 			fmt.Println("No films in struct")
 			return
