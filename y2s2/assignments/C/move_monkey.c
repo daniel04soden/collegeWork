686f00b3cc0df79185db7aaf66a974e5241a60e5
daniel04soden <dsoden09@gmail.com>
2025-05-06 20:44:15 +0100

    started ooa study

--- y2s2/assignments/C/move_monkey.c
+++ y2s2/assignments/C/move_monkey.c
@@ -1,31 +1,24 @@
-#include <stdlib.h>
 #include "hungry_monkey.h"
+#include <stdlib.h>
 
 struct monkey_action move_monkey(int field[][FIELD_WIDTH], void *monkey_state) {
-    struct monkey_action retval;
-    int final_move;
-    int monkey_col;
-    for (int j = 0; j < FIELD_WIDTH; j++) {
-        if (field[FIELD_HEIGHT - 1][j] == MONKEY_VAL) {
-            monkey_col = j;
-            break;
-        }
+  struct monkey_action retval;
+  int final_move;
+  int monkey_col;
+  for (int j = 0; j < FIELD_WIDTH; j++) {
+    if (field[FIELD_HEIGHT - 1][j] == MONKEY_VAL) {
+      monkey_col = j;
+      break;
     }
+  }
 
-    int above = field[FIELD_HEIGHT][monkey_col];
-    int left = field[FIELD_HEIGHT][monkey_col-1];
-    int right = field[FIELD_HEIGHT][monkey_col+1];
-    
+  int above = field[FIELD_HEIGHT - 2][monkey_col];
+  int left = field[FIELD_HEIGHT - 2][monkey_col - 1];  // Diag left
+  int right = field[FIELD_HEIGHT - 2][monkey_col + 1]; // Diag right
 
-    if (monkey_state == NULL) {
-      monkey_state = malloc(sizeof(int));
-    }
-    
-    
+  if (monkey_state == NULL) {
+    monkey_state = malloc(sizeof(int));
+  }
 
-    
-    
-
-    
-    return retval;
+  return retval;
 }
---
+++ y2s2/notes/Study/OOA/Models/Domain Models.md
@@ -0,0 +1,12 @@
+# Domains
+- The subject area to which the user applies a program is the **domain** of the software
+- Hardest part of software is understanding this domain **(critical complexity)** 
+- **Problem domain** - the area of expertise that needs to be examined to solve a problem
+-  If measuring KPI on sales, wouldn't measure ability to pick up rubbish, task of the day for workers but not what you're tasked with measuring.
+
+
+# Domain Model
+
+- **Domain Models** illustrate the conceptual classes in the problem domain
+- Concepts discovered tend to lead to design classes
+- Not real world conceptual classes, not of software components and not set of diagrams describing software classes or software objects with responsibilities
