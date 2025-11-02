4693063c98b3a0ca0b26c66d4984e3c9d6d0d4ac
Daniel Soden <dsoden09@gmail.com>
2025-11-02 21:51:19 +0000

    started php project

---
+++ Assignments/year3/phpProjectOne/MyWebsite/index.php
@@ -0,0 +1,48 @@
+<?php
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="styles.css">
+	<title>Student Profile Viewer</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Student Profile Viewer</h1>
+        </div>
+    <div class="container">
+        <nav>
+           <a href="">Home</a>
+           <a href="">Search</a>
+           <a href="">Log out</a>
+        </nav>
+    </div>
+    <div class="container">
+        <h2>Current Selected Student</h2>
+        <a href="/">
+            <img src="avatars/002.jpg">
+        </a>
+    </div>
+    <div class="container">
+        <div>
+            <ul>
+                <a href="/">
+                    <img src="avatars/002.jpg" alt="Image of Primary selected student">
+                </a>
+                <img src="avatars/003.jpg" alt="img of student">
+                <img src="avatars/004.jpg" alt="img of student">
+                <img src="avatars/005.jpg" alt="img of student">
+                <img src="avatars/006.jpg" alt="img of student">
+                <img src="avatars/007.jpg" alt="img of student">
+                <img src="avatars/008.jpg" alt="img of student">
+                <img src="avatars/009.jpg" alt="img of student">
+            </ul>
+        </div>
+    </div>
+  </body>
+</html>
